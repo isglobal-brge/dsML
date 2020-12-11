23b7ac0b25a8a19091ff026a62508b83bdc9026b
Xavier Escribà Montagut <escri11@yahoo.es>
2020-12-11 10:56:12 +0100

    cart proof of concept

---
+++ R/prepare_dataset_treeDS.R
@@ -0,0 +1,35 @@
+#' Title
+#'
+#' @param object 
+#' @param questions 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+prepare_dataset_treeDS <- function(object, questions){
+  
+  questions <- unserialize(questions)
+  
+  for(i in 1:nrow(questions)){
+    if(questions[i,4] == "numerical"){
+      value <- as.numeric(substr(questions[i,3], 7, nchar(questions[i, 3])))
+      if(substr(questions[i,3], 1, 5) == "lower"){
+        object <- object[object[[questions[i,2]]] <= value, ]
+      }
+      else{
+        object <- object[object[[questions[i,2]]] > value, ]
+      }
+    }
+    else{
+      value <- substr(questions[i,3], 7, nchar(questions[i, 3]))
+      if(substr(questions[i,3], 1, 5) == "equal"){
+        object <- object[object[[questions[i,2]]] == value, ]
+      }
+      else{
+        object <- object[object[[questions[i,2]]] != value, ]
+      }
+    }
+  }
+  return(object)
+}
